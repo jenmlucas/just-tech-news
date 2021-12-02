9ade54c753a0674b01c02db292af8f414294c1a0
jenmlucas <jenmlucas1@gmail.com>
2021-12-02 07:18:13 -0700

    added hash password functions

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 //create our USER model
 class User extends Model { }
@@ -45,6 +46,18 @@ User.init(
         }
     },
     {
+        hooks: {
+            //set up befopreCreate lifecycle "hook" functionality 
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                    return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         // pass in our imported sequelize connection (the direct connection to our database)
         sequelize,
         // don't automatically create createdAt/updatedAt timestamp fields (just tells when database was created and updated with timestamps)
