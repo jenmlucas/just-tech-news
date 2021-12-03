29f410c4548b28261aeb766f8ee1847f6568f83f
jenmlucas <jenmlucas1@gmail.com>
2021-12-03 06:32:08 -0700

    correct syntax error

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
-const Vote = require('./Vote');
+const Vote = require('./vote');
 const Comment = require('./Comment');
 
 //create associations
